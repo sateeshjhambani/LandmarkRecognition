e09bd15f014099b3c96b8e268912d362a50adfc0
Sateesh <sateesh.jh@gmail.com>
2024-02-10 22:17:34 +0400

    Added the UI and camera permissions

--- app/src/main/java/com/sateeshjh/landmarkrecognition/data/TFLiteLandmarkClassifier.kt
+++ app/src/main/java/com/sateeshjh/landmarkrecognition/data/TFLiteLandmarkClassifier.kt
@@ -67,10 +67,10 @@ class TFLiteLandmarkClassifier(
 
     private fun getOrientationFromRotation(rotation: Int): ImageProcessingOptions.Orientation {
         return when (rotation) {
-            Surface.ROTATION_0 -> ImageProcessingOptions.Orientation.RIGHT_TOP
+            Surface.ROTATION_270 -> ImageProcessingOptions.Orientation.BOTTOM_RIGHT
             Surface.ROTATION_90 -> ImageProcessingOptions.Orientation.TOP_LEFT
             Surface.ROTATION_180 -> ImageProcessingOptions.Orientation.RIGHT_BOTTOM
-            else -> ImageProcessingOptions.Orientation.BOTTOM_RIGHT
+            else -> ImageProcessingOptions.Orientation.RIGHT_TOP
         }
     }
 }
